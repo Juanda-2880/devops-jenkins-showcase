bf1527efc313cfad9a6d756d328ee366bb440391
Juan David <juandapv995@gmail.com>
2026-05-03 09:01:39 -0500

    Fixing Errors 1.0

--- Terraform/modules/compute/EC2.tf
+++ Terraform/modules/compute/EC2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "EC2-Jenkins" {
     instance_type = "c7i-flex.large"
     vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
     key_name               = "jenkins-key"
-    user_data = file("${path.module}/../../../scripts/jenkins-install.sh")
+    user_data = file("${path.module}/../../scripts/jenkins-install.sh")
 
     tags = {
         Description = "EC2 para alojar Jenkins"
